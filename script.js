80569a8dd4eabf47cdd15d20a880538d9c2fe814
thoriq <thoriqnfaizal@gmail.com>
2018-09-09 22:49:05 +0700

    Exercising switch case and ternary

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+// function isUserValid(bool){
+// 	return bool;
+// }
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+// var answer = isUserValid(true) ? "You may enter" : "Access denied";
 
-	css.textContent = body.style.background + ";";
+// var automatedAnswer = 
+// 	"your account # is " + (isUserValid(false) ? "1234" : "Not Available");
+
+// 	function condition() {
+// 		if (isUserValid(true)) {
+// 			return "You may enter";
+// 		} else {
+// 			return "You can't enter";
+// 		}
+// 	}
+
+// 	var answer2 = condition();
+
+function moveCommand(direction) {
+	var whatHappens;
+	switch (direction){
+		case "forward":
+			whatHappens = "you encounter a monster";
+			break;
+		case "back":
+			whatHappens = "you arrived home";
+			break;
+		case "right":
+			whatHappens = "you found a river";
+			break;
+		case "left":
+			whatHappens = "you run into a troll";
+			break;
+		default: 
+			whatHappens = "please enter a valid direction"
+	}
+	return whatHappens;
 }
-
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
