34a937e60e9f5a9f1439ca9ae139019d0b984c41
thoriq <thoriqnfaizal@gmail.com>
2018-09-09 12:32:37 +0700

    Trying for, do and do while

---
+++ script.js
@@ -0,0 +1,30 @@
+var todos = [
+  "clean room",
+  "brush teeth",
+  "exercise",
+  "Study javascript",
+  "eat healthy",  
+];
+
+var todosLength = todos.length;
+
+// for
+
+// for (var i=0; i < todosLength; i++){
+//   todos.pop();
+// } 
+
+// while
+
+// var counterOne = 10;
+// while (counterOne > 0) {
+//   console.log(counterOne);
+//   counterOne++
+// }
+
+// do
+var counterTwo = 10
+do {
+  console.log(counterTwo);
+  counterTwo--;
+} while (counterTwo > 0);
