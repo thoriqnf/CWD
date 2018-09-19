a8e306dee25e6d95b35819d4d3670314586b9eb2
thoriq <thoriqnfaizal@gmail.com>
2018-09-19 13:54:43 +0700

    Learning how to use function debugger

--- script.js
+++ script.js
@@ -1,36 +1,12 @@
-// .padStart()
-// .padEnd()
-'Turtle'.padStart(10);
-'Turtle'.padEnd(10);
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(accumulator, array) => {
+		console.log('array', array);
+		console.log('accumulator', accumulator);
+		return accumulator.concat(array)
+	}, []);
 
-// can give coma at the end
-const fun = (a,b,c,d, ) => {
-	console.log(a);
-}
-fun (1,2,3,4,)
-
-Object.values
-Object.entries
-Object.MediaKeySession
-
-let obj = {
-	username0: 'Santa',
-	username1: 'Rudolf',
-	username2: 'Mr thoriq'
-}
-
-Object.keys(obj).forEach((key, index) => {
-	console.log(key, obj[key]);
-})
-
-Object.values (obj).forEach(value => {
-	console.log(value);
-}) 
-
-Object.entries(obj).forEach(value => {
-	console.log(value);
-}) 
-
-Object.entries(obj).map(value => {
-	return value[1] + value[0].replace('username', '');
-}) 
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+	(accumulator, array) => {
+		debugger;
+		return accumulator.concat(array)
+	}, []);
