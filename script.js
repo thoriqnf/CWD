bdd12e48efab0b9cf46b1287e970ec93bb41b7c6
thoriq <thoriqnfaizal@gmail.com>
2018-09-20 21:58:08 +0700

    Modules

--- script.js
+++ script.js
@@ -1,52 +1,17 @@
-const a = 1 ;
-const b = 10;
-const c = 100;
+Modules 
 
-javascript have 2 engine called: mempry heap and callstack
+// inline script
+inline like at html code 
 
-javascript is a single threaded language that can be non-blocking
+// sctipt tag
+modular to another script tag
 
-Single thread means so the callstack only can do one thing at one time, from the top to down until empty
+//iife 
+immediately-invoked function Expression
 
-console.log('1');
-console.log('2');
-console.log('3');
-
-const one = () => {
-	const two = () => {
-		console.log('4');
-	}
-	console.log('1');
-}
-
-//call stack
-
-//recursion
-function foo() {
-	foo()
-}
-
-foo()
-make foo over and over again until max stack
-
-///////////////////////
-
-console.log('1');
-setTimeout (() => {
-	console.log('2');
-}, 2000);
-console.log('3');
+// browserify
+Totally module bundler
 
 
-// call stack
-
-// web api
-
-// callback queue
-
-// event loop
-check call stack empty, if empty can stop
-
-element.addEventListener('click', () => {
-	console.log('click');
-})
+// es6 + webpack
+Can use export default module
