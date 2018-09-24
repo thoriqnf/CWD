b8546efdbb90011e08dcf6736303f32b4591c4f5
thoriq <thoriqnfaizal@gmail.com>
2018-09-24 21:59:51 +0700

    - Using componentdidmount() - fetching api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import CardList from './CardList'
-import { robots } from './robots';
+import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,25 +7,36 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {});
   }
 
   onSearchChange = (event) => {
     this.setState({searchfield: event.target.value  })
   }
+
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return (
-      <div className="tc ">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots} />
-      </div>
-      )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading dong</h1>
+    } else {
+      return (
+        <div className="tc ">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      ) 
+    }
   }
 }
 
