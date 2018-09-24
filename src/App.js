fc2ef69b49ab9bae3e81dafbe2eab459feab9bd1
thoriq <thoriqnfaizal@gmail.com>
2018-09-24 22:43:05 +0700

    - Adding scrollable search box

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
-
+import Scroll from './Scroll';
 class App extends Component {
   constructor(){
     super()
@@ -15,7 +15,7 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => {});
+    .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -33,7 +33,9 @@ class App extends Component {
         <div className="tc ">
           <h1 className="f1">Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filteredRobots} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
         </div>
       ) 
     }
