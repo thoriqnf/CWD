4fb529471bc0b7eb1553be55e4318ca0bafa2eef
thoriq <thoriqnfaizal@gmail.com>
2018-09-24 09:07:49 +0700

    Making component card - Styling card - Adding dynamic data from robots.js - Adding dynamic props at card - Call card in index.js using array

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {robots} from './robots';
 
-ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja '}/>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+  </div>, document.getElementById('root'));
 registerServiceWorker();
