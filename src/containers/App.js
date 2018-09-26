98ef68f4adbdb7d27e9258527622cada25cf286c
thoriq <thoriqnfaizal@gmail.com>
2018-09-26 20:07:56 +0700

    Restructuring folder and optimize

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import './App.css';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import '../containers/App.css';
 class App extends Component {
   constructor(){
     super()
@@ -23,13 +23,14 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (this.state.robots.length === 0) {
-      return <h1>Loading dong</h1>
-    } else {
-      return (
+
+    return !robots.length ?
+      <h1>Loading dong</h1> :
+       (
         <div className="tc ">
           <h1 className="f1">Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -37,8 +38,7 @@ class App extends Component {
             <CardList robots={filteredRobots} />
           </Scroll>
         </div>
-      ) 
-    }
+      );
   }
 }
 
