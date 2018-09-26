9dc36e7fa52372b8a63b75c7438149056709e612
thoriq <thoriqnfaizal@gmail.com>
2018-09-26 20:44:04 +0700

    Adding Error Boundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,19 +2,24 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardComponent = robots.map((user, i) => {
-    return <Card 
-      key={i} 
-      id={robots[i].id} 
-      name={robots[i].name} 
-      email={robots[i].email} 
-    />
-  })
-  return(
+  if (true) {
+   throw new Error('NOOOOOO!'); 
+  }return(
     <div>
-      {cardComponent}
+      {
+        robots.map((user, i) => {
+          return ( 
+            <Card 
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+            />
+          )
+        })
+      }
     </div>
-  )
+  );
 }
 
 export default CardList
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import '../containers/App.css';
+import ErrorBoundry from '../components/ErrorBoundry';
 class App extends Component {
   constructor(){
     super()
@@ -35,7 +36,9 @@ class App extends Component {
           <h1 className="f1">Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
