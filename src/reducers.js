e4606b50822fff350933a46c25ef9de94244a9fa
thoriq <thoriqnfaizal@gmail.com>
2018-09-28 14:09:17 +0700

    Adding React-redux Adding action and reducers

---
+++ src/reducers.js
@@ -0,0 +1,16 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js'
+
+const initialState = {
+  searchField: ''
+}
+
+export const searchRobots = (state=initialState, action={}) => {
+  switch(action.type) {
+    case: CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, searchField: action.paylaod );
+      // Bisa gunain object destructuring or object spread operator
+      // return {.AudioProcessingEvent..state, searchField: action.payload }
+    default:
+      return state;
+  }
+}
