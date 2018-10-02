e35630da0c076bc10d674b38be4d3421c47182d6
thoriq <thoriqnfaizal@gmail.com>
2018-10-02 14:47:37 +0700

    Adding redux-thunk Get async action

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,42 +6,36 @@ import Scroll from '../components/Scroll';
 import '../containers/App.css';
 // import ErrorBoundry from '../components/ErrorBoundry
 
-import { setSearchField } from '../actions'
+import { setSearchField, requestRobots } from '../actions'
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
  
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value ))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value )),
+    onRequestRobots: () => dispatch (requestRobots())
   }
 }
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      robots: []
-    }
-  
-  }
+
   componentDidMount() {
-    // console.log(this.props.store.getState());
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ robots: users }));
+   this.props.onRequestRobots();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } =this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
 
-    return !robots.length ?
+    return isPending ?
       <h1>Loading dong</h1> :
        (
         <div className="tc ">
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import { searchRobots }from './reducers';
+import { searchRobots, requestRobots }from './reducers';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger))
+
+const rootReducers = combineReducers({ searchRobots, requestRobots})
+
+const store = 
+  createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
               <Provider store={store}>
