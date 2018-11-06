b63a71f78c91181039b473a17d06aafcdd11370d
thoriq <thoriqnfaizal@gmail.com>
2018-11-06 20:54:40 +0700

    Adding react-tilt for Logo

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-{/*        <Logo />
+        <Logo />{/* 
         <ImageLinkForm />
 <FaceRecognition />*/}
       </div>
