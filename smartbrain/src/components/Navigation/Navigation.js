84935450d742ad95eb269f296ed9a1845aba8a6f
thoriq <thoriqnfaizal@gmail.com>
2018-11-06 20:15:05 +0700

    Removing unnecessary file and setup early

---
+++ smartbrain/src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+  return(
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <p>Sign Out</p>
+    </nav>
+  )
+}
+
+export default Navigation;
