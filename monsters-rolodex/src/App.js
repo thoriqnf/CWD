a07bf3a9d50947f1d9ecc696d461a9ff0d0a5fb5
thoriq <thoriqnfaizal@gmail.com>
2019-07-03 19:57:35 +0700

    Start mastering react

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "hello thoriq"
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,1 @@
+1. key buat tau dia mana aja yang ada update dan perlu di render ulang, jadi ga perlu re render ulang semuanya
