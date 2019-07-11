fb59417ac8952c3d5d83c5b4e516c8c94a389d15
thoriq <thoriqnfaizal@gmail.com>
2019-07-11 12:43:27 +0700

    Adding new fproject for homepages

---
+++ e-commerce/src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../component/directory/index";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
